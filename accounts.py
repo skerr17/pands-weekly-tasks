f7a042e028688f0f9e0028826e24b2c658370a23
Stephen Kerr <kerrstephen17@gmail.com>
2025-02-19 00:03:34 +0000

    added in a bank account number max of 20

--- accounts.py
+++ accounts.py
@@ -5,8 +5,10 @@
 # Author: Stephen Kerr
 '''
 # Assumptions:
- 1. For the extra requirement is that the bank account number must be at least 4 digits long
- 2. The bank account number can be any length over 4 digits (no limit)
+ 1. For the extra requirement is that the bank account number must be more than 4 digits long. (minimum 4 digits)
+ 2. The bank account number character lenght limit is 20.  (Maximum 20 digits)
+ 3. The user will enter a valid bank account number between the 4 digit min and 20 didgit max.
+ 4. The bank account number will be a string.
 '''
 # prompt the user for their bank account number
 bank_account_number = str(input("Please enter your bank account number: "))
@@ -16,9 +18,14 @@ bank_account_number = str(input("Please enter your bank account number: "))
 # and replace the rest with X characters
 bank_account_number_lenght = len(bank_account_number)
 
-# check if the account number is less than 4 digits
+# check if the account number is less than 4 digits and no more than 20 digits
 if bank_account_number_lenght < 4:
     print("The bank account number must be at least 4 digits long")
+
+elif bank_account_number_lenght >= 20:
+    # check the account number is not more than 20 digits
+    print("The bank account number must be less than 20 digits long")
+
 else:
     # get the last 4 digits of the account number
     last_4_digits = bank_account_number[-4:]
@@ -26,5 +33,5 @@ else:
     # calculate the number of x characters to be displayed
     x_characters = bank_account_number_lenght - 4
 
-# print the account number in the new format
-print(f"The last 4 digits of your bank account number are: {x_characters * 'X'}{last_4_digits}")
+    # print the account number in the new format
+    print(f"The last 4 digits of your bank account number are: {x_characters * 'X'}{last_4_digits}")
